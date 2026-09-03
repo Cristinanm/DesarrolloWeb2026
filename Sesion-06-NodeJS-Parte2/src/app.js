c70c99eb72a6cb56c964dcffb6c2b4d03a54b752
Cristina <anajeram@miumg.edu.gt>
2026-09-02 20:03:04 -0600

    feat: agregar leer lineas

--- Sesion-06-NodeJS-Parte2/src/app.js
+++ Sesion-06-NodeJS-Parte2/src/app.js
@@ -85,7 +85,18 @@ export async function filtrarLogs(origen, destino, texto) {
  * @returns {Promise<string[]>}
  */
 export async function leerLineas(ruta) {
-     throw new Error('Not implemented: filtrarLogs');
+    const stream = createReadStream(ruta, {encoding:'utf-8'});
+
+  let contenido = '';
+
+  for await (const chunk of stream){
+    contenido += chunk;
+  }
+
+  return contenido
+    .split('\n')
+    .map(linea => linea.trim())
+    .filter(linea => linea.length > 0);
 }
 
 /**
